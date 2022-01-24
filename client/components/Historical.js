2b5db077061ad974aae1b5bff91d8b0e4a225e2c
Blaine Interrante <interrante.blaine@gmail.com>
2022-01-24 09:26:30 -0500

    feat(chart filters on date): adds radio buttons to filter the viewable period on the chart to 7D, 30D, 60D, 180D, MaxD

--- client/components/Historical.js
+++ client/components/Historical.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import {
   VictoryAxis,
@@ -11,7 +11,17 @@ import {
 
 const Historical = () => {
   const { historical: data } = useSelector((state) => state);
-  const [zoom, setZoom] = useState({});
+  const [chartDays, setChartDays] = useState(7);
+  const [high, setHigh] = useState([]);
+  const [low, setLow] = useState([]);
+
+  function handleRadioButtonChange(e) {
+    setChartDays(e.target.value);
+    if (data) {
+      setLow(data.filter((e, i) => i <= chartDays).reduce((a, b) => (a.close <= b.close) ? a : b, {}));
+      setHigh(data.filter((e, i) => i <= chartDays).reduce((a, b) => (a.close >= b.close) ? a : b, {}));
+    }
+  }
 
   return (
     <div>
@@ -19,28 +29,37 @@ const Historical = () => {
         <div>Historical data is not available</div>
       ) : (
         <div>
+          <div onChange={handleRadioButtonChange}>
+            <input type="radio" value={7} name="days" defaultChecked /> 7D
+            <input type="radio" value={30} name="days" /> 30D
+            <input type="radio" value={60} name="days" /> 60D
+            <input type="radio" value={180} name="days" /> 180D
+            <input type="radio" value={data.length} name="days" /> {`${data.length}D`}
+          </div>
+          <div>
+            <p>{`Low: ${low.date} ${low.close}`}</p>
+            <p>{`High: ${high.date} ${high.close}`}</p>
+          </div>
           <VictoryChart
             theme={VictoryTheme.material}
             width={550}
             height={300}
             scale={{ x: "time" }}
-            containerComponent={
-              <VictoryZoomContainer
-                responsive={false}
-                zoomDimension="x"
-                zoomDomain={zoom}
-                onZoomDomainChange={setZoom}
-              />
-            }
           >
             <VictoryLine
               style={{
-                data: { stroke: "#c43a31" },
-                parent: { border: "1px solid #ccc" },
+                data: { stroke: "#2aea20" },
+                parent: { border: "1px solid #000000" },
               }}
-              data={data}
+              data={data.filter((e, i) => i <= chartDays)}
               x="date"
               y="close"
+            />
+            <VictoryAxis
+              crossAxis
+            />
+            <VictoryAxis
+              dependentAxis
             />
           </VictoryChart>
         </div>
